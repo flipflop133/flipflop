db53393bb922d460c63c303b7ace1dbb86b87c82
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2014-07-23 01:00:36 +0200

    PM: Create PM workqueue if runtime PM is not configured too
    The PM workqueue is going to be used by ACPI PM notify handlers
    regardless of whether or not runtime PM is configured, so move
    it out of #ifdef CONFIG_PM_RUNTIME.
    
    Do that in three places in the ACPI device PM code.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -623,7 +623,6 @@ static struct attribute_group attr_group = {
 	.attrs = g,
 };
 
-#ifdef CONFIG_PM_RUNTIME
 struct workqueue_struct *pm_wq;
 EXPORT_SYMBOL_GPL(pm_wq);
 
@@ -633,9 +632,6 @@ static int __init pm_start_workqueue(void)
 
 	return pm_wq ? 0 : -ENOMEM;
 }
-#else
-static inline int pm_start_workqueue(void) { return 0; }
-#endif
 
 static int __init pm_init(void)
 {
